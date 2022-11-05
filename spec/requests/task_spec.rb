393a6738a96f662aacaa018702ba52f8e2b263cc
21mad <ilmir.abyzov@mail.ru>
2022-11-05 13:33:47 +0300

    Fixed root_path in tests

--- spec/requests/task_spec.rb
+++ spec/requests/task_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Tasks", type: :request do
   describe "GET /input" do
     it "returns http success" do
-      get "/task/input"
+      get root_path
       expect(response).to have_http_status(:success)
     end
   end
